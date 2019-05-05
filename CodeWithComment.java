fbd98db9fc3cee8929f6b68a42537b43396c892e
Joshua0407 <joshua104216@gmail.com>
2019-05-05 15:15:45 +0800

    為我的程式加上註解

---
+++ CodeWithComment.java
@@ -0,0 +1,11 @@
+//以下就是我們所填寫的第一個Java程式
+public class CodeWithComment {
+    public static void main(String[] args) {
+        // 到上面這兩行都是固定的骨架
+
+        // 在 main)()方法中就是我們要執行的程式
+        System.out.println("我的第一個Java程式");
+
+        // 以下都是固定的程式骨架
+    }// main () 方法的結束括號
+}// CodeWithComment 的結束括號
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-1. javac programName.java => 編譯
-2. java programName => 執行
+1. javac 程式名稱.java => 編譯
+遇到中文編碼失敗　=>
+
+javac -encoding UTF8 程式名稱.java
+
+
+2. java 程式名稱 => 執行
+
+ctrl + S => 存檔
+alt + shift + F =>打完之後格式化
