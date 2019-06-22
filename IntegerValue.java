6ebbe13e83cff20ea437e903294f83f90fc17951
Joshua0407 <joshua104216@gmail.com>
2019-06-22 22:30:13 +0800

    使用整數值

---
+++ IntegerValue.java
@@ -0,0 +1,14 @@
+public class IntegerValue {
+    public static void main(String[] args) {
+        System.out.println("10  1357         =" + 1357);
+        int i = 0b10011001; // int
+
+        System.out.println("0b1001001 =" + i);
+
+        long l = 0XADEF; // lone
+        System.out.println("16  0XADEF   =" + i);
+
+        short s = 01357; // short 
+        System.out.println("8 01357    = " + s);
+    }
+}
