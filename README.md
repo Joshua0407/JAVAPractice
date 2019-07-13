fa2853b519a2d56f77cda8ac4c54015140cd1b91
Joshua0407 <joshua104216@gmail.com>
2019-07-13 16:30:46 +0800

    Number 使用

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ file not found => 找不到檔案
 
 ctrl + S => 存檔
 alt + shift + F =>打完之後格式化
+
+3. 上傳到git hub
+使用sourcetree
+a. 選擇staginf all
+b. 選擇下方push changes imemdiately to orgin/master
+c. 點選commit
---
+++ UsingNumber.java
@@ -0,0 +1,12 @@
+public class UsingNumber {
+    public static void main(String[] args) {
+        short s = -13666;
+        System.out.println("s = " + s );
+
+        long l = 2_135_482_789L;
+        System.out.println(" 1 = " + l);
+
+int  i = 0b1100_0110_0011_1010;
+System.out.println(" i = " + i);
+    }
+}
