17bcc128f6f0f9025a39c75edb4a9b77c6219d5d
Joshua0407 <joshua104216@gmail.com>
2019-07-13 18:00:43 +0800

    使用跳脫程式

---
+++ EscapeValue.java
@@ -0,0 +1,12 @@
+public class EscapeValue{
+
+
+public static void main(String[] args) {
+    char  ch ='\u5b57';
+    System.out.println("ch : " + ch);
+
+    ch ='\\';
+
+    System.out.println("ch : " + ch);
+   }
+}
